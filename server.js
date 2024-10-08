b44a1f14fd0406e5d46dd2953fc1dad22537af32
kienle-k <kevin.k@gmx.net>
2024-10-08 11:02:09 +0200

    Added first files to start the project

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+
+// Initialisiere Express
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+// Statische Dateien aus dem "public"-Ordner bereitstellen
+app.use(express.static('public'));
+
+// WebSocket-Verbindung
+io.on('connection', (socket) => {
+    console.log('Ein Benutzer hat sich verbunden');
+
+    // Nachricht empfangen und an alle Clients weiterleiten
+    socket.on('chat message', (msg) => {
+        io.emit('chat message', msg);
+    });
+
+    // Wenn ein Benutzer die Verbindung trennt
+    socket.on('disconnect', () => {
+        console.log('Ein Benutzer hat die Verbindung getrennt');
+    });
+});
+
+// Server starten
+server.listen(3000, () => {
+    console.log('Server läuft auf http://localhost:3000');
+});
