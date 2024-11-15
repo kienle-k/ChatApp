6e86f90318c6169f558a85a081e3307f9c463b31
Constantin-Junior Fugaru <constantin-junior.fugaru@nokia.com>
2024-11-15 18:30:20 +0100

    Created contacts logic
    Also deleted placeholder contacts

--- public/scripts/contacts_logic.js
+++ public/scripts/contacts_logic.js
@@ -23,6 +23,31 @@ CURRENT_CHAT_GROUP = null;      // Shows which group chat is currently displayed
 //     }
 socket.on('response-chat-history', (rows) => {
     console.log("RECEIVED CHATS:", rows.messages);
+    let contact_username;
+
+    for (const message of rows.messages) {
+        if (message.group_name == null) {
+            if (message.sender_username == MY_USER) {
+                contact_username = message.receiver_username;
+            } else {
+                contact_username = message.sender_username;
+            }
+
+            let contact_list = document.getElementById("contacts");
+            contact_list.insertAdjacentHTML('beforeend', 
+                `<li class="contact-container">
+                  <button type="button" class="contact-profile-button">
+                    <img src="/images/profile.jpg">
+                  </button>
+                  <button type="button" class="choose-contact-button">
+                    <div class="contact">${contact_username}</div>
+                  </button>
+                </li>`
+            );
+        } else {
+            console.log("bro really thought")
+        }
+    }
     // TODO
     // CJ FU
     // Logik für das Generieren / Regenerieren der Divs (je nachdem ob nur neu laden / hinzufügen zur liste)
