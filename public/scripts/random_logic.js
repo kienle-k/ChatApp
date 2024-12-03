0b4e5e466c2cd7a8f2627ae3151fc70c1258c8ef
Jonas Brenk <jonas.brenk@nokia.com>
2024-12-03 20:34:21 +0100

    Better search bar, random chat is highlighted in colors

--- public/scripts/random_logic.js
+++ public/scripts/random_logic.js
@@ -4,7 +4,24 @@ document.getElementById('random-chat-button').addEventListener('click', async ()
         const data = await response.json();
         if (data.success) {
             //alert(`Starting chat with ${data.user.username}`);
+
+            prevID = 
             addContact(data.user.id, data.user.username, data.user.profile_picture); // Starte den Chat mit der zufälligen Person, zeige den Kontakt direkt an
+            
+            setTimeout(() => {
+                const tmp = document.getElementById("messages").style.border;
+                document.getElementById("messages").style.border = "2px solid red";
+                setTimeout(() => {
+                    document.getElementById("messages").style.border = "2px solid purple";
+                }, 250);
+                setTimeout(() => {
+                    document.getElementById("messages").style.border = "2px solid blue";
+                }, 500);            
+                setTimeout(() => {
+                    document.getElementById("messages").style.border = tmp;
+                }, 750);
+            }, 600);
+            
             //choosePersonalChat(data.user.id); // Starte den Chat mit der zufälligen Person
         } else {
             alert('Failed to start random chat');
