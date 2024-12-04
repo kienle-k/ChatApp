68ce49f4cf9a58a60b391621deedee94a79798c8
Koikralle2k24 <sebastiandomack@web.de>
2024-12-04 11:24:08 +0100

    working register pv
    Password visibilty button is working on register page

--- public/scripts/password_visibility_logic.js
+++ public/scripts/password_visibility_logic.js
@@ -1,4 +1,4 @@
-// Toggle password visibility
+// Toggle password visibility register page
 document.getElementById('toggle-password').addEventListener('click', function () {
     const passwordField = document.getElementById('password');
     const type = passwordField.getAttribute('type') === 'password' ? 'text' : 'password';
@@ -11,4 +11,15 @@ document.getElementById('toggle-confirm-password').addEventListener('click', fun
     const type = confirmPasswordField.getAttribute('type') === 'password' ? 'text' : 'password';
     confirmPasswordField.setAttribute('type', type);
     this.textContent = type === 'password' ? '👁️' : '🙈';
+});
+
+//login page
+document.addEventListener('DOMContentLoaded', function () {
+    // Toggle password visibility
+    document.getElementById('toggle-password').addEventListener('click', function () {
+        const passwordField = document.getElementById('password');
+        const type = passwordField.getAttribute('type') === 'password' ? 'text' : 'password';
+        passwordField.setAttribute('type', type);
+        this.textContent = type === 'password' ? '👁️' : '🙈';
+    });
 });
