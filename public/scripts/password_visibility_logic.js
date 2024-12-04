5c670b083e518a0cb15a3218f2151201778ddb6c
Koikralle2k24 <sebastiandomack@web.de>
2024-12-04 11:46:04 +0100

    Working in Login
    Funktioniert in Login und Register

--- public/scripts/password_visibility_logic.js
+++ public/scripts/password_visibility_logic.js
@@ -13,13 +13,3 @@ document.getElementById('toggle-confirm-password').addEventListener('click', fun
     this.textContent = type === 'password' ? '👁️' : '🙈';
 });
 
-//login page
-document.addEventListener('DOMContentLoaded', function () {
-    // Toggle password visibility
-    document.getElementById('toggle-password').addEventListener('click', function () {
-        const passwordField = document.getElementById('password');
-        const type = passwordField.getAttribute('type') === 'password' ? 'text' : 'password';
-        passwordField.setAttribute('type', type);
-        this.textContent = type === 'password' ? '👁️' : '🙈';
-    });
-});
